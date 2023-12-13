1b111de9ac7d5e29958d678e93f9675f0ac98e4c
Fábián Varga <23280129+br0kenpixel@users.noreply.github.com>
2023-12-13 19:45:04 +0100

    remove debug print

--- callbacks.go
+++ callbacks.go
@@ -1,10 +1,7 @@
 package main
 
-import "fmt"
-
 func RunCode() {
 	py_code := code.Text
-	fmt.Printf("Executing: %s \n", py_code)
 	result := Exec(uint8(runtimeSelector.SelectedIndex()), py_code)
 
 	output.Text = result
